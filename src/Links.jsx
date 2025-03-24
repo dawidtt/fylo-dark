74aa417f41747f3515dd3b3cb2cc5372aea07d40
dawidtt <dawid.gorszka@gmail.com>
2025-03-24 20:31:48 +0100

    Create Links component

---
+++ src/Links.jsx
@@ -0,0 +1,18 @@
+import PropTypes, { string } from "prop-types";
+
+function Links({ links }) {
+  return (
+    <div className="flex flex-col text-white gap-2.5 my-8">
+      {links.map((link) => (
+        <a key={crypto.randomUUID()} href="#">
+          {link}
+        </a>
+      ))}
+    </div>
+  );
+}
+
+Links.PropTypes = {
+  links: PropTypes.arrayOf(string),
+};
+export default Links;
